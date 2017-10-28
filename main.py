2c35b5f881b5238f2f0d1bdb1f44f8bcdc9a70cc
Alex Lee <525210804@qq.com>
2017-10-28 15:37:52 +0800

    Create main.py
    Added main file of the module

---
+++ main.py
@@ -0,0 +1,7 @@
+from module_utility.learning_markets.video_download import start_extract_learning_markets
+
+def main():
+    #Download the latest learning markets video
+    start_extract_learning_markets(True)
+
+main()
