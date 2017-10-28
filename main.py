4731487f50129d6a69af6697e26fe0b8c17aa6e7
Alex Lee <525210804@qq.com>
2017-10-28 16:05:37 +0800

    Update main.py
    First to update data and then download the video

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from module_utility.learning_markets.video_download import start_extract_learnin
 def main():
     #Download the latest learning markets video
     start_extract_learning_markets(True)
+    start_extract_learning_markets(False)
 
 main()
