1325b8ef6983ab846735e3be61de5a6efc7f0c56
Alex Lee <525210804@qq.com>
2017-10-30 15:13:12 +0800

    Update test_disk_space.py
    FIxed the float show error issue.

--- file_test/test_disk_space.py
+++ file_test/test_disk_space.py
@@ -1,7 +1,7 @@
 import os
 disk = os.statvfs("/root")
-percent = 0.0
-percent = float((disk.f_blocks - disk.f_bfree) / disk.f_blocks)
+percent = float(disk.f_blocks - disk.f_bfree) / float(disk.f_blocks)
 print("total number of blocks in filesystem: " + str(disk.f_blocks))
 print("total number of free blocks: " + str(disk.f_bfree))
-print("%.5f"%(percent))
+percent = format(percent, ".4f")
+print(percent)
