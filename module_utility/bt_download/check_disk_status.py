4c800472ef3125952017f6afe5dd031fec6391ac
Alex Lee <525210804@qq.com>
2017-11-07 19:39:48 +0800

    Update check_disk_status.py
    UPdated code to caculate the disk space

--- module_utility/bt_download/check_disk_status.py
+++ module_utility/bt_download/check_disk_status.py
@@ -1,9 +1,12 @@
 import os
-def check_disk_percentage(file_size):
+MAX_DISK_SPACE = 20*1024*1024*1024
+
+def check_max_acceptable_size():
     disk = os.statvfs("/root")
-    percent = float(disk.f_blocks - disk.f_bfree + file_size) / float(disk.f_blocks)
+    percent = float(disk.f_blocks - disk.f_bfree) / float(disk.f_blocks)
     print("total number of blocks in filesystem: " + str(disk.f_blocks))
     print("total number of free blocks: " + str(disk.f_bfree))
     percent = format(percent, ".4f")
     percent = float(percent)
-    return percent
+
+    max_file_size = ((1.0 - percent)*MAX_DISK_SPACE*0.7) / 2.0
