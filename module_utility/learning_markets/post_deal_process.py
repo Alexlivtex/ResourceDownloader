eff72b6f3d86a407804914fd671dc9a3886815c0
Wei Li <525210804@qq.com>
2017-11-11 17:28:49 +0800

    Modified the logic of time out function

--- module_utility/learning_markets/post_deal_process.py
+++ module_utility/learning_markets/post_deal_process.py
@@ -4,8 +4,11 @@ from ..timeout import timeout
 
 MAX_WAIT_TIME = 15*60
 
+def upload(path):
+    bp = ByPy()
+    bp.upload(path)
+
 @timeout(MAX_WAIT_TIME)
 def post_process():
-    bp = ByPy()
-    bp.upload("file_download")
+    upload("file_download")
     os.system("rm -rf file_download/learning_markets_video/*")
