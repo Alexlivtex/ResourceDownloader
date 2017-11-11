9ae2f897425bc2b1ab8043e4a39a11b8a5a3b236
Wei Li <525210804@qq.com>
2017-11-11 23:35:38 +0800

    Changed the time out function setting

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from module_utility.learning_markets.video_download import start_extract_learning_markets
 from module_utility.learning_markets.post_deal_process import post_process
 from bypy import ByPy
-from module_utility.timeout import timeout
 import os
+
+from functools import wraps
+import errno
+import os
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
 
 MAX_TIME_LEARNING_MARKETS = 15*60
 MAX_TIME_BT_DOWNLOAD = 45*60
