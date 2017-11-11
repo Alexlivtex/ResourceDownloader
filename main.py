660474f96603440de214ec6cfc670a57fb23a149
Wei Li <525210804@qq.com>
2017-11-11 17:46:44 +0800

    Fixed a timeout error

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from module_utility.learning_markets.video_download import start_extract_learning_markets
 from module_utility.learning_markets.post_deal_process import post_process
+from bypy import ByPy
+from module_utility.timeout import timeout
+import os
 
+MAX_TIME_LEARNING_MARKETS = 15*60
+MAX_TIME_BT_DOWNLOAD = 45*60
 
+@timeout(MAX_TIME_LEARNING_MARKETS)
+def upload_learning_markets(path):
+    bp = ByPy()
+    bp.upload(path)
 
 def main():
     #Download the latest learning markets video
@@ -9,6 +18,7 @@ def main():
     start_extract_learning_markets(False)
     while True:
         try:
+            upload_learning_markets("file_download")
             post_process()
             break
         except:
--- module_utility/learning_markets/post_deal_process.py
+++ module_utility/learning_markets/post_deal_process.py
@@ -1,14 +1,3 @@
 import os
-from bypy import ByPy
-from ..timeout import timeout
-
-MAX_WAIT_TIME = 15*60
-
-def upload(path):
-    bp = ByPy()
-    bp.upload(path)
-
-@timeout(MAX_WAIT_TIME)
 def post_process():
-    upload("file_download")
     os.system("rm -rf file_download/learning_markets_video/*")
