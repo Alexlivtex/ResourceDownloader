7d494dd51fa04f216676d27518af9b86dfee522c
Alex Lee <525210804@qq.com>
2017-11-13 19:46:07 +0800

    Update compress_folder.py
    Updated compress folder

--- module_utility/bt_download/compress_folder.py
+++ module_utility/bt_download/compress_folder.py
@@ -3,9 +3,17 @@ import os
 compression_level = 5
 import shutil
 
-def compress_folder(folder_name, dst_path):
+def walk_file_path(path):
     file_list = []
-    for i in os.listdir(folder_name):
-        file_list.append(os.path.join(folder_name, i))
-    pyminizip.compress_multiple(file_list, os.path.join(dst_path, "test"), "133456", compression_level)
+    for fpathe, dirs, fs in os.walk(path):
+        for f in fs:
+            file_list.append(os.path.join(fpathe, f))
+            print os.path.join(fpathe, f)
+
+    return file_list
+
+def compress_folder(folder_name, dst_path):
+    file_list = walk_file_path(folder_name)
+    pyminizip.compress_multiple(file_list, dst_path, "133456", compression_level)
     shutil.rmtree(folder_name)
+
