de31c60d04a6f86d8da3758bd9639efeda6bd8bb
Alex Lee <525210804@qq.com>
2017-11-28 18:49:50 +0800

    Update torrent_transform.py
    Reduced the max thread count

--- module_utility/bt_download/torrent_transform.py
+++ module_utility/bt_download/torrent_transform.py
@@ -19,7 +19,7 @@ pickle_torrent_parse_data = os.path.join("file_config", "bt_download", "data_tot
 finished_transform_list = []
 finished_transform_data = os.path.join("file_config", "bt_download", "finished_transform.pickle")
 
-max_thread_count = 20
+max_thread_count = 15
 
 
 def magnet2torrent(magnet, output_name=None):
