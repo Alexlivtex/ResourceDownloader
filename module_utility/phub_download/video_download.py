1f1207afa66b4d446a592c3c4cf565eed7b409a3
Wei Li <525210804@qq.com>
2017-12-08 23:48:27 +0800

    Fixed a logic that lead by local variable.

--- module_utility/phub_download/video_download.py
+++ module_utility/phub_download/video_download.py
@@ -9,7 +9,7 @@ pickle_data_bak = os.path.join("file_config", "bt_download", "hub_hash_total_bak
 
 start_url = os.path.join("file_config", "bt_download", "url_start_here")
 
-total_has_list = []
+total_hash_list = []
 
 def extract_link(url):
     response = requests.get(url)
@@ -18,17 +18,18 @@ def extract_link(url):
     for a in soup.find_all('a', href=True):
         if len(a['href'].split("viewkey=")) > 1:
             hash_value = (a['href'].split("viewkey="))[-1][:15]
-            if not hash_value in total_has_list:
-                total_has_list.append(hash_value)
-                if len(total_has_list) % 50 == 0:
+            if not hash_value in total_hash_list:
+                total_hash_list.append(hash_value)
+                if len(total_hash_list) % 50 == 0:
                     f_hash_total = open(pickle_data, "wb")
-                    pickle.dump(total_has_list, f_hash_total)
+                    pickle.dump(total_hash_list, f_hash_total)
                     f_hash_total.close()
                     shutil.copy(pickle_data, pickle_data_bak)
 
 
 
 def begin_hub_download():
+    global total_hash_list
     url_video_prefix = ""
     if os.path.exists(pickle_data):
         try:
@@ -51,7 +52,7 @@ def begin_hub_download():
     url_video_prefix = url_lines[0].split("viewkey=")[0]
     f_url_list.close()
 
-    for hash_index in total_has_list:
+    for hash_index in total_hash_list:
         link = url_video_prefix + hash_index
         print(link)
 
