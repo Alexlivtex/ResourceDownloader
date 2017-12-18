b19fe994999a365a1998256f4618f9578008cf7c
Alex Lee <525210804@qq.com>
2017-12-18 14:22:34 +0800

    Update check_disk_status.py
    Added code to avoid importing error

--- module_utility/bt_download/check_disk_status.py
+++ module_utility/bt_download/check_disk_status.py
@@ -11,3 +11,12 @@ def check_max_acceptable_size():
 
     max_file_size = ((1.0 - percent)*MAX_DISK_SPACE*0.7) / 2.0
     return max_file_size
+
+def check_disk_percentage():
+    disk = os.statvfs("/root")
+    percent = float(disk.f_blocks - disk.f_bfree) / float(disk.f_blocks)
+    print("total number of blocks in filesystem: " + str(disk.f_blocks))
+    print("total number of free blocks: " + str(disk.f_bfree))
+    percent = format(percent, ".4f")
+    percent = float(percent)
+    return percent
