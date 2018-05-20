8f8125293918f4adc9844dead7f49d2837220919
Wei Li <525210804@qq.com>
2018-05-20 10:49:38 +0800

    Added code to close the driver when issue appeared

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ def main():
         options.add_argument("--no-sandbox")
         driver = webdriver.Chrome("/usr/bin/chromedriver",chrome_options=options)
 
-    extract_source_asis_nocode(driver, url, data["cl1024"][0]["id"], data["cl1024"][0]["password"], CL_1024_PATH)
+    try:
+        extract_source_asis_nocode(driver, url, data["cl1024"][0]["id"], data["cl1024"][0]["password"], CL_1024_PATH)
+    except:
+        driver.close()
     #analyze_link(CL_1024_PATH, driver)
 
 
