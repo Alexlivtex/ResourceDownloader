3b1c452822d327d6321626d151406f820c34b1f4
Alex Lee <525210804@qq.com>
2018-06-03 14:06:33 +0800

    Update ph_extract.py
    Fixed a issue that can not parse subpage

--- module_utility/source_extract/ph_extract.py
+++ module_utility/source_extract/ph_extract.py
@@ -11,35 +11,38 @@ url_data_base = dict()
 url_error_list = list()
 
 
-def extract_link(url, data_name, bak_data_name, data_error_name):
+def extract_link(url, url_base, data_name, bak_data_name, data_error_name):
     response = requests.get(url)
     # print(response.content)
     soup = bs.BeautifulSoup(response.content, "lxml")
     for a in soup.find_all('a', href=True):
         if len(a['href'].split("viewkey=")) > 1:
             hash_value = (a['href'].split("viewkey="))[-1][:15]
-            if not hash_value in url_data_base:
-                #viewCount = soup.find_all('span', {'class': 'count'})[0].text
-                if len(soup.find_all('span', {'class': 'count'})) > 0:
-                    viewCount = soup.find_all('span', {'class': 'count'})[0].text
-                else:
-                    if not hash_value in url_error_list:
-                        url_error_list.append(hash_value)
-                    continue
-                viewCount = "".join(viewCount.split(','))
-                url_data_base[hash_value] = viewCount
-                print("************************{} has viewed  {} times************************".format(hash_value,
-                                                                                                       viewCount))
-                if len(url_data_base) % 50 == 0:
-                    f_hash_total = open(data_name, "wb")
-                    pickle.dump(url_data_base, f_hash_total)
-                    f_hash_total.close()
-                    shutil.copy(data_name, bak_data_name)
-
-                    with open(data_error_name, "wb") as f:
-                        pickle.dump(url_error_list, f)
+            try:
+                sub_response = requests.get(url_base + "/view_video.php?viewkey=" + hash_value)
+                sub_soup = bs.BeautifulSoup(sub_response.content, "lxml")
+            except:
+                url_error_list.append(hash_value)
+                continue
+            # viewCount = soup.find_all('span', {'class': 'count'})[0].text
+            if len(sub_soup.find_all('span', {'class': 'count'})) > 0:
+                viewCount = sub_soup.find_all('span', {'class': 'count'})[0].text
             else:
-                print("hash value of {} has already existed!".format(hash_value))
+                if not hash_value in url_error_list:
+                    url_error_list.append(hash_value)
+                continue
+            viewCount = "".join(viewCount.split(','))
+            url_data_base[hash_value] = viewCount
+            print("************************{} has viewed  {} times************************".format(hash_value,
+                                                                                                   viewCount))
+            if len(url_data_base) % 50 == 0:
+                f_hash_total = open(data_name, "wb")
+                pickle.dump(url_data_base, f_hash_total)
+                f_hash_total.close()
+                shutil.copy(data_name, bak_data_name)
+
+                with open(data_error_name, "wb") as f:
+                    pickle.dump(url_error_list, f)
 
 
 def extract_ph_source(driver, paramList):
@@ -66,7 +69,7 @@ def extract_ph_source(driver, paramList):
         for item in list(url_data_base):
             full_url = url + "/view_video.php?viewkey=" + item
             print(full_url)
-            extract_link(full_url, data_name, data_name_bak, data_name_error)
+            extract_link(full_url, url, data_name, data_name_bak, data_name_error)
             time.sleep(2)
         '''
         try:
