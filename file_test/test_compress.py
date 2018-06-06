52ea66e19c2271792cfdf0f26a5472362c03a300
Wei Li <525210804@qq.com>
2018-06-06 21:54:53 +0800

    Deleted old project files

--- file_test/test_compress.py
+++
@@ -1,4 +0,0 @@
-import pyminizip
-compression_level = 5
-pyminizip.compress("total.pickle", "test.zip", "123456", compression_level)
-pyminizip.compress("test.zip", "final.zip", "123456", compression_level)
--- module_utility/learning_markets/post_deal_process.py
+++
@@ -1,3 +0,0 @@
-import os
-def post_process():
-    os.system("rm -rf file_download/learning_markets_video/*")
