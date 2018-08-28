13e7573c4851af2b4096d3a549bdec92b3b24617
Alex Lee <525210804@qq.com>
2018-08-28 19:39:09 +0800

    Update main.py
    Enter password during each login time

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import json
 import os
+import getpass
 from DBHelper import dbConnect
 
 def main():
     data = {}
+    needModify = input("Need to modify the database information? Y/N : ")
+    if str(needModify).lower() == "y":
+        os.remove("config.json")
+    elif str(needModify).lower() == "n":
+        print("Just keep the current configuation!")
+    else:
+        print("Invalid input, please re-input the config information!")
+        os.remove("config.json")
+
     if os.path.exists("config.json"):
         with open("config.json", "r") as f:
             data = json.load(f)
             host = data["host"]
             port = data["port"]
             user = data["user"]
-            password = data["password"]
+            password = getpass.getpass("Please input the password of the user {} : ".format(user))
     else:
-        host = input("Please input the host address :")
-        port = input("Please input the database port :")
-        user = input("Please input the user name of the database :")
-        password = input("Please input the password of the database :")
+        host = input("Please input the host address : ")
+        port = input("Please input the database port : ")
+        user = input("Please input the user name of the database : ")
+        password = getpass.getpass("Please input the password of the database : ")
         data["host"] = host
         data["port"] = port
         data["user"] = user
-        data["password"] = password
         with open("config.json", "w") as f:
             json.dump(data, f, ensure_ascii=False, indent=4, separators=(",", ":"))
 
