59980fe49b538e58060631432e74c7712b78b6e1
Alex Lee <525210804@qq.com>
2018-09-17 11:00:09 +0800

    Update DBHelper.py
    Added interface to convert the db encoding

--- Utils/DBHelper.py
+++ Utils/DBHelper.py
@@ -64,3 +64,23 @@ def fetchData(db, fetchSQL):
     myCursor = db.cursor()
     myCursor.execute(fetchSQL)
     return myCursor
+
+def db_convert(p_host, p_passwd, p_user, p_dbname):
+    host = p_host
+    passwd = p_passwd
+    user = p_user
+    dbname = p_dbname
+
+    db = pymysql.connect(host=host, user=user, passwd=passwd, db=dbname)
+    cursor = db.cursor()
+
+    cursor.execute("ALTER DATABASE `%s` CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci'" % dbname)
+
+    sql = "SELECT DISTINCT(table_name) FROM information_schema.columns WHERE table_schema = '%s'" % dbname
+    cursor.execute(sql)
+
+    results = cursor.fetchall()
+    for row in results:
+        sql = "ALTER TABLE `%s` convert to character set DEFAULT COLLATE DEFAULT" % (row[0])
+        cursor.execute(sql)
+    db.close()
